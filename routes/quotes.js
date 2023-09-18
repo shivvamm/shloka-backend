431b97a379f31d3b796dbec98e5ba3f27ee971f4
Shivam Pandey <58168590+shivvamm@users.noreply.github.com>
2023-09-18 23:04:46 +0000

    Diff approach sending svg file

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 const gitaShloks = require("./../public/shlokas/gitashloks.json");
 const sanskritSlogan = require('./../public/shlokas/shloksvalid');
 const vidurNitiShloks = require('./../public/shlokas/shloksvalid');
@@ -22,17 +23,17 @@ if (!data) {
 }
 const respo = data.sloka;
 
+const attributes = { fill: 'red', stroke: 'black' };
+const options = { x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes };
 
+const svg = textToSVG.getSVG(respo, options);
+fs.writeFileSync("test.txt",svg)
+console.log(svg)
 
 router.get("/svg", (req, res) => {
     try {
-        const attributes = { fill: 'red', stroke: 'black' };
-        const options = { x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes };
-
-        const svg = textToSVG.getSVG(respo, options);
-
-        console.log(svg);
-        return res.status(200).send(svg);
+        res.setHeader('Content-Type', 'image/svg+xml');
+        res.sendFile('./index.svg');
     } catch (e) {
         console.log(e);
         return res.status(500).json({
